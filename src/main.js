b87af10ec7de6ea7bd38834f53978212a8cf1d30
xhaeffer <subhansyahsp@gmail.com>
2024-07-10 03:17:51 +0700

    change api host, install a package

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const routes = require('./routes/router');
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: '0.0.0.0',
   });
   
   await server.register(Jwt);
