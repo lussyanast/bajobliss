15b4fa7d3a2fc691b2d3c86107631920ba129d25
lussyanast <lussyanast@gmail.com>
2024-07-10 20:37:47 +0700

    update view profile page

--- src/scripts/views/pages/profile.js
+++ src/scripts/views/pages/profile.js
@@ -1,22 +1,43 @@
 const Profile = {
   async render() {
     return `
-      <div class="content"></div>
+      <div class="content">
+        <div class="profile-content">
+          <div class="profile">
+            <h1>My Profile</h1>
+            <div class="profile-details">
+              <div class="profile-image">
+                <img src="" alt="Profile Image">
+              </div>
+              <div class="profile-info">
+                <h2>Lorem Ipsum</h2>
+                <p>Email: lorem@ipsum.com</p>
+                <p>Phone: +628123456789</p>
+                <p>Gender: Perempuan</p>
+                <div class="address">
+                  <h3>Alamat</h3>
+                  <p>Jl. Coklat IX xx, rw xx</p>
+                  <p>Kode Pos</p>
+                  <p>Kota Jakarta</p>
+                  <p>Provinsi Jawa Barat</p>
+                  <p>Indonesia</p>
+                </div>
+                <button class="edit-profile-btn">Edit Profile</button>
+              </div>
+            </div>
+          </div>
+          <a class="logout" href="#home">Log Out</a>
+        </div>
+      </div>
     `;
   },
 
   async afterRender() {
-    const displayProfile = () => {
+    const displayProfile = async () => {
       const content = document.querySelector('.content');
-      let htmlContent = '';
-
-      htmlContent += `
-        <div class="profile">
-            <h1>Profile</h1>
-        </div>
-      `;
-
-      content.innerHTML = htmlContent;
+      if (content) {
+        content.innerHTML = await this.render();
+      }
     };
 
     displayProfile();
