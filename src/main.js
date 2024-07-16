c76b9403a55fa496fc279f0c190feba611dd14d3
xhaeffer <subhansyahsp@gmail.com>
2024-07-16 19:44:31 +0700

    enable cors

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 3000,
     host: '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
   
   await server.register(Jwt);
